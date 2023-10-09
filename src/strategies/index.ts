bbebb87a52dd4dbe8240b7e2858f4bf85675a34f
RoccoCocco <rokokokeza@gmail.com>
2023-10-09 09:53:08 +0200

    chore: remove unused strategies

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,4 +1,2 @@
 export { BruteforceSortStrategy } from "./bruteforceSortStrategy";
 export { PriorityGroupStrategy } from "./priorityGroupStrategy";
-export { ValueSortStrategy } from "./valueSortStrategy";
-export { WeightedScoreStrategy } from "./weightedScoreStrategy";
